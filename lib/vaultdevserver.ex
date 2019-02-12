0972c171562e59ce019d5089de578e6281a74ba2
Jamie Hewland <jhewland@gmail.com>
2019-02-12 15:01:05 +0200

    mix new vaultdevserver

---
+++ lib/vaultdevserver.ex
@@ -0,0 +1,18 @@
+defmodule Vaultdevserver do
+  @moduledoc """
+  Documentation for Vaultdevserver.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vaultdevserver.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vaultdevserver_test.exs
@@ -0,0 +1,8 @@
+defmodule VaultdevserverTest do
+  use ExUnit.Case
+  doctest Vaultdevserver
+
+  test "greets the world" do
+    assert Vaultdevserver.hello() == :world
+  end
+end
